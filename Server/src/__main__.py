425c16a36fd1d1aff5fa417179c6f6b156327e62
jonny <aes1993rich@gmail.com>
2024-11-29 21:27:19 +0500

    add ENV for start app

--- Server/src/__main__.py
+++ Server/src/__main__.py
@@ -1,6 +1,14 @@
+import os
+
 import uvicorn
 
 from src.application import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)  # noqa: S104
+    uvicorn.run(
+        app,
+        host=os.getenv("SERVER_HOST", "0.0.0.0"),  # noqa: S104
+        port=int(os.getenv("SERVER_PORT", "8000")),
+        ws_max_queue=int(os.getenv("SERVER_WS_MAX_QUEUE", "256")),
+        ws_max_size=int(os.getenv("SERVER_WS_MAX_SIZE", "16777216")),
+    )
