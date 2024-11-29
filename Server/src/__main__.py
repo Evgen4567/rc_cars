bf8ca479062a5e7f65ab7757e93d57d0878a6f37
jonny <aes1993rich@gmail.com>
2024-11-29 21:56:27 +0500

    add more queue for ws

--- Server/src/__main__.py
+++ Server/src/__main__.py
@@ -3,4 +3,4 @@ import uvicorn
 from src.application import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, timeout_keep_alive=60)  # noqa: S104
+    uvicorn.run(app, host="0.0.0.0", port=8000, timeout_keep_alive=60, ws_max_queue=256)  # noqa: S104
